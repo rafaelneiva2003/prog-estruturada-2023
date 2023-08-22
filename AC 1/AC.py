78b654d7cf6e597114ad1a0e6a3c71501ce3a9c2
Rafael Neiva Marques de Lima <112084805+rafaelneiva2003@users.noreply.github.com>
2023-08-22 16:51:55 -0300

    Add files via upload

---
+++ AC 1/AC.py
@@ -0,0 +1,8 @@
+a = 1
+b = -6
+c = 8
+
+delta = b**2 - 4 * a *c
+raiz1 = (-b + delta ** 0.5) / (2 *a)
+raiz2 = (-b + delta ** 0.5) / (2 *a)
+print("Raiz 1:", delta)
---
+++ AC 1/AC2.py
@@ -0,0 +1,2 @@
+ano = int(input("Informe ano: "))
+print(ano % 4 = 0 and ano%100!=0)
